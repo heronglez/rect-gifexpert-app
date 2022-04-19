7a18b9f7095de54e77f6f320482704b0f2f7754c
hgonzalez <hgonzalez@enerfueltech.com>
2022-04-19 14:38:44 -0700

    nuevo test dentro de useFetchGifs.test.js

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,15 +3,29 @@ import { useFetchGifs } from "../../hooks/useFetchGifs"
 
 describe('Pruebas en el hook useFetchGifs', () => { 
 
-    test('debe de retornar el estado inicial', () => {
+    test('debe de retornar el estado inicial', async() => {
 
 
-        const { result } = renderHook(() => useFetchGifs('euphoria'))
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('euphoria'))
         const { data, loading } = result.current;
+
+        await waitForNextUpdate();
 
         expect( data ).toEqual([])
         expect( loading ).toBe(true)
 
+    });
+
+    test('debe de retornar un arreglo de imagenes y el loading en false', async () => {
+
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('euphoria'))
+
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect( data.length ).toBe(10)
+        expect( loading ).toBe(false)
     })
 
 })
